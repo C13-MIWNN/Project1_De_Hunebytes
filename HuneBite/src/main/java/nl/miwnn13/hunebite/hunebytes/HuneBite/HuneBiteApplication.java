6acce1d28d9b4b205bdcbdd692142a098bce9108
Justin <justin_lamberts@hotmail.com>
2024-04-08 15:00:18 +0200

    Initial commit

---
+++ HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/HuneBiteApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn13.hunebite.hunebytes.HuneBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuneBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuneBiteApplication.class, args);
+	}
+
+}
---
+++ HuneBite/src/test/java/nl/miwnn13/hunebite/hunebytes/HuneBite/HuneBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn13.hunebite.hunebytes.HuneBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuneBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
