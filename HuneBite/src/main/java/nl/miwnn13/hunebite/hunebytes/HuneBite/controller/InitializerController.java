7ef6f25ff8c07956e035750c74ab27666f643782
Justin <justin_lamberts@hotmail.com>
2024-04-10 10:41:40 +0200

    Initialize controller added and JPArepository added to Ingredient

--- HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/InitializerController.java
+++ HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/InitializerController.java
@@ -1,5 +1,78 @@
-package nl.miwnn13.hunebite.hunebytes.HuneBite.controller;/**
+package nl.miwnn13.hunebite.hunebytes.HuneBite.controller;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.model.Ingredient;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.model.Recipe;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.model.RecipeBook;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.repositories.BookRepository;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.repositories.IngredientRepository;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.repositories.RecipeRepository;
+import nl.miwnn13.hunebite.hunebytes.HuneBite.repositories.TagRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.awt.print.Book;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
  * @author Justin Lamberts
  * Purpose for the class
- **/public class InitializerController {
+ **/
+    @SuppressWarnings("SameReturnValue")
+    @Controller
+    public class InitializeController {
+
+        private final BookRepository bookRepository;
+        private final IngredientRepository ingredientRepository;
+        private final RecipeRepository recipeRepository;
+        private final TagRepository tagRepository;
+
+    public InitializeController(BookRepository bookRepository, IngredientRepository ingredientRepository, RecipeRepository recipeRepository, TagRepository tagRepository) {
+        this.bookRepository = bookRepository;
+        this.ingredientRepository = ingredientRepository;
+        this.recipeRepository = recipeRepository;
+        this.tagRepository = tagRepository;
+    }
+
+
+    @GetMapping("/initialize")
+    private String initializeDB() {
+
+
+        RecipeBook favorites = makeRecipeBook("Favorites");
+
+        Recipe defaultRecipe = makeRecipe("Gekookt ei");
+
+        Ingredient defaultIngredient = makeIngredient("Ei");
+
+        return "redirect:/";
+    }
+
+    private RecipeBook makeRecipeBook(String title) {
+        RecipeBook recipeBook = new RecipeBook();
+        recipeBook.setTitle(title);
+
+        Set<RecipeBook> authorSet = new HashSet<>();
+        authorSet.add(recipeBook);
+        RecipeBook.setRecipeBook(authorSet);
+
+        bookRepository.save(recipeBook);
+        return recipeBook;
+    }
+
+    private Recipe makeRecipe(String name) {
+        Recipe recipe = new Recipe();
+        recipe.setname = new Recipe();
+        recipeRepository.save(recipe);
+        return recipe;
+    }
+
+    private Ingredient makeIngredient(String ingredient) {
+        Ingredient defaultingredient = new Ingredient();
+        defaultingredient.setingredient();
+        ingredientRepository.save(defaultingredient);
+        return defaultingredient;
+    }
 }
+
