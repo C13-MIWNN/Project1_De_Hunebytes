cb6f05380bef2e63c972b8e17aa156e681a78370
Tim <tim-bulder@live.nl>
2024-04-12 16:49:54 +0200

    Added recipeForm and implemented the general layout in recipeForm and recipeDetail

--- HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/RecipeController.java
+++ HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/RecipeController.java
@@ -1,11 +1,15 @@
 package nl.miwnn13.hunebite.hunebytes.HuneBite.controller;
 
+
 import nl.miwnn13.hunebite.hunebytes.HuneBite.model.Recipe;
 import nl.miwnn13.hunebite.hunebytes.HuneBite.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -18,6 +22,7 @@ import java.util.Optional;
 @Controller
 public class RecipeController {
     private final RecipeRepository recipeRepository;
+
     public RecipeController(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
@@ -32,5 +37,29 @@ public class RecipeController {
 
         model.addAttribute("recipeToBeShown", recipe.get());
         return "recipeDetail";
+    }
+
+    @GetMapping("/recipe/new")
+    private String showRecipeForm(Model model) {
+        model.addAttribute("recipe", new Recipe());
+
+
+        return "recipeForm";
+    }
+
+    @PostMapping("/recipe/new")
+    private String saveRecipe(@ModelAttribute("recipe") Recipe recipeToBeSaved, BindingResult result) {
+        if (recipeToBeSaved.getRecipeId() == null
+                && recipeRepository.findByRecipeTitle(recipeToBeSaved.getRecipeTitle()).isPresent()) {
+            return "redirect:/recipe/new";
+        }
+
+        if (!result.hasErrors()) {
+            recipeRepository.save(recipeToBeSaved);
+        }
+
+        String recipeTitle = recipeToBeSaved.getRecipeTitle();
+
+        return "redirect:/recipe/detail/" + recipeTitle;
     }
 }
