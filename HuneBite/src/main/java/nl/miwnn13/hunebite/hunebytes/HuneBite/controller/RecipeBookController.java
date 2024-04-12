7bb54afaa5c01602667f73d95cecc7dbbc5e3d4a
Justin <justin_lamberts@hotmail.com>
2024-04-12 16:30:11 +0200

    adjusted settings in RecipeBookController and RecipeBook, further updated the frontend and book overviewpages and added a book form to add new books

--- HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/RecipeBookController.java
+++ HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/RecipeBookController.java
@@ -1,12 +1,16 @@
 package nl.miwnn13.hunebite.hunebytes.HuneBite.controller;
 
+import nl.miwnn13.hunebite.hunebytes.HuneBite.model.Ingredient;
 import nl.miwnn13.hunebite.hunebytes.HuneBite.model.Recipe;
 import nl.miwnn13.hunebite.hunebytes.HuneBite.model.RecipeBook;
 import nl.miwnn13.hunebite.hunebytes.HuneBite.repositories.RecipeBookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -22,21 +26,37 @@ public class RecipeBookController {
         this.recipeBookRepository = recipeBookRepository;
     }
 
-@GetMapping({"/"})
+    @GetMapping({"/"})
     public String showOverviewPage(Model model) {
-        model.addAttribute("RecipeBook", recipeBookRepository.findAll());
+        model.addAttribute("allRecipeBooks", recipeBookRepository.findAll());
         return "homepageOverview";
     }
     @GetMapping("/recipeBook/detail/{SpecificRecipeBook}")
     private String showRecipeDetail(@PathVariable("SpecificRecipeBook") String SpecificRecipeBook, Model model) {
-        Optional<RecipeBook> recipeBookName = recipeBookRepository.findByRecipeBookName(SpecificRecipeBook);
+        Optional<RecipeBook> recipeBook = recipeBookRepository.findByRecipeBookName(SpecificRecipeBook);
 
-        if (SpecificRecipeBook.isEmpty()) {
+        if (recipeBook.isEmpty()) {
             return "redirect:/";
         }
 
-        model.addAttribute("recipeBookToBeShown", recipeBookName.get().getRecipeBookName());
+        model.addAttribute("recipeBookToBeShown", recipeBook.get());
         return "RecipeBookOverviewPage";
     }
+    @GetMapping("/recipebook/new")
+    private String showIngredientForm(Model model) {
+        model.addAttribute("NewRecipeBook", new RecipeBook());
+        return "RecipeBookForm";
+    }
+
+    @PostMapping("/recipebook/new")
+    private String AddNewIngredient(@ModelAttribute("recipeBook")
+                                    RecipeBook recipeBook, BindingResult recipeBookResult) {
+
+        if (!recipeBookResult.hasErrors()) {
+            recipeBookRepository.save(recipeBook);
+        }
+        return "redirect:/";
+    }
+
 }
  
