5ca4e6abb1a4bdd0a5c2706753095ba88ff63827
Justin <justin_lamberts@hotmail.com>
2024-04-25 10:28:17 +0200

    added Tests to test the countRecipes() method in RecipeBook

--- HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/InitializeController.java
+++ HuneBite/src/main/java/nl/miwnn13/hunebite/hunebytes/HuneBite/controller/InitializeController.java
@@ -81,7 +81,7 @@ import java.util.*;
         return user;
     }
 
-    private RecipeBook makeRecipeBook(String Name, Recipe recipe) {
+    protected RecipeBook makeRecipeBook(String Name, Recipe recipe) {
         RecipeBook recipeBook = new RecipeBook();
         recipeBook.setRecipeBookName(Name);
 
